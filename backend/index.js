e184b9cfdc78f7729d0515a8dcd525d886387ecf
Jackylai01 <94669444+Jackylai01@users.noreply.github.com>
2023-02-02 15:37:04 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ const corsOptions = {
   origin: [
     "https://sandbox-api-pay.line.me/v3/payments/request",
     "http://localhost:8080",
+    "https://jacky-clothing-e-commerce.onrender.com"
     "http://localhost:3000",
     "http://localhost:3001",
   ],
