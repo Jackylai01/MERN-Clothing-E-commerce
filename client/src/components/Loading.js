8d553e2df03f44033713652c400d5f0781fad065
Jackylai01 <94669444+Jackylai01@users.noreply.github.com>
2023-02-07 17:08:11 +0800

    Create Loading.js

---
+++ client/src/components/Loading.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Loading = styled.div`
+  border: 10px solid #f3f3f3;
+  border-top: 10px solid #3498db;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Spinner = () => {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+};
+
+export default Spinner;
